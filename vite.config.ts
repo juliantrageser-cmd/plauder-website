2b4a159d9bed09a8a656f3473c0057d8952968b5
Julian T <juliantrageser@googlemail.com>
2026-03-13 18:29:47 +0100

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/plauder-website/",
+  base: mode === "development" ? "/" : "/plauder-website/",
   server: {
     host: "::",
     port: 8080,
